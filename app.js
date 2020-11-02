b588c5e0fa33e5f3f231ec8b3da38d6cb70f2f6e
Yasir Tobbileh <yasur96@gmail.com>
2020-11-02 17:48:46 +0200

    trying to implement 2 player entry

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-for (let i = 1; i <= 9; i++){
-
+for (let i = 1; i <= 9; i++) {
     var img = document.createElement("img");
-    img.setAttribute("Src","https://www.flaticon.com/svg/static/icons/svg/566/566294.svg");
+    img.setAttribute("Src", "https://www.flaticon.com/svg/static/icons/svg/566/566294.svg");
     img.setAttribute("width", "90");
     img.setAttribute("height", "90");
     img.setAttribute("id", `img${i}`);
     // console.log(img)
     img.addEventListener("click", renderImg)
-    document.getElementById(`#${i}`).appendChild(img); 
+    document.getElementById(`#${i}`).appendChild(img);
 }
 
-function renderImg(event){
-    console.log(event)
-
-}
-
-function StartGame(){
+var count = 0;
+function renderImg(event) {
     
-   
+    var select = ["https://www.flaticon.com/premium-icon/icons/svg/2997/2997911.svg", "https://www.flaticon.com/svg/static/icons/svg/16/16894.svg"]
+    event.target.setAttribute("src", select[1]);
+    event.target.removeEventListener("click", renderImg)
+    
+    if (true){
+        count++;
+        console.log(count)
+    }
+    // var currentImg = event.target.currentSrc;
+    // currentImg = "https://www.flaticon.com/premium-icon/icons/svg/2997/2997911.svg";
+    // console.log(img)
+    console.log(count)
+    
+}
+
+
+function StartGame() {
 
 }
+
+// var newGame = renderImg();
 
