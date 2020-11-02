26552a5739f223e28e296e0e605db6c47df69fdb
Yasir Tobbileh <yasur96@gmail.com>
2020-11-02 16:29:32 +0200

    adding event listner

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ for (let i = 1; i <= 9; i++){
     img.setAttribute("Src","https://www.flaticon.com/svg/static/icons/svg/566/566294.svg");
     img.setAttribute("width", "90");
     img.setAttribute("height", "90");
-    // console.log(`#${i}`)
-    var x = `#${i}`;
-    console.log(document.getElementById(x))
-    document.getElementById(`#${i}`).appendChild(img);
+    img.setAttribute("id", `img${i}`);
+    // console.log(img)
+    img.addEventListener("click", renderImg)
+    document.getElementById(`#${i}`).appendChild(img); 
 }
 
+function renderImg(event){
+    console.log(event)
+
+}
 
 function StartGame(){
-
+    
+   
 
 }
 
