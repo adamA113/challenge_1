77de0036adaccdfaa76e8ac477f9ad8f1cdc217e
Yasir Tobbileh <yasur96@gmail.com>
2020-11-02 15:51:34 +0200

    defining the 3*3 boxes

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+for (let i = 1; i <= 9; i++){
 
+    var img = document.createElement("img");
+    img.setAttribute("Src","https://www.flaticon.com/svg/static/icons/svg/566/566294.svg");
+    img.setAttribute("width", "90");
+    img.setAttribute("height", "90");
+    // console.log(`#${i}`)
+    var x = `#${i}`;
+    console.log(document.getElementById(x))
+    document.getElementById(`#${i}`).appendChild(img);
+}
+
+
+function StartGame(){
+
+
+}
 
